6ec5cf4cd1de7bc0d61ed114c0d1772675394685
miche <mendezmichelle857@gmail.com>
2026-09-12 10:20:33 -0600

    corregir estructura de MainActivity para mostrar el botón flotante

--- desafio2DSM/app/src/main/java/com/example/appturistica/MainActivity.kt
+++ desafio2DSM/app/src/main/java/com/example/appturistica/MainActivity.kt
@@ -4,9 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
 import com.example.appturistica.ui.AppNavigation
 import com.example.appturistica.ui.theme.AppTuristicaTheme
 
@@ -16,9 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AppTuristicaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    AppNavigation()
-                }
+                AppNavigation()
             }
         }
     }
